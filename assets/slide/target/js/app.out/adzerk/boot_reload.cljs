d3c1592aba16c5009200b7375b8f6e0930d3edb7
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-01-28 23:22:32 +0800

    clean
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- assets/slide/target/js/app.out/adzerk/boot_reload.cljs
+++
@@ -1,2 +0,0 @@
-(ns adzerk.boot-reload (:require [adzerk.boot-reload.client :as client] slide.app))
-(client/connect "ws://localhost:49452" {:on-jsload (fn* [] (slide.app/init))})
--- js/app.out/boot/cljs/main12888.cljs
+++
@@ -1,3 +0,0 @@
-(ns boot.cljs.main12888 (:require adzerk.boot-cljs-repl adzerk.boot-reload slide.app))
-
-(do (slide.app/init))
